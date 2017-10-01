3db8333c949b16d9da28608d03c17f82d4ff0724
Ryan Adriano <raime910@msn.com>
2017-10-01 02:26:56 -0500

    my-first-app initial setup

---
+++ my-first-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+// rma - this is where we unlock features in our angular app.
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
