5bdc37f8b30bd4bf6ae80d6d7fb23f8b4eb1f81d
Ryan Adriano <raime910@msn.com>
2017-10-01 04:29:49 -0500

    review main.ts and app.module.ts

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', /* selector matches the html element
+                           where this component will be injected
+                           into the document. */
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
+  name = 'My First App';
 }
--- my-first-app/src/main.ts
+++ my-first-app/src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// This is the first file that is executed by Ng.
+
 if (environment.production) {
   enableProdMode();
 }
 
+// Bootstraps/start Ng app by passing AppModule
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+/* main.ts
+    -> app.module.ts
+      -> app.component.ts
+*/
