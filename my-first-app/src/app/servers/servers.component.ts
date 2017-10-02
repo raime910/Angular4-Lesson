f3ba49645371b36f5fb2c3d5a8fc1728bd9d7fa8
Ryan Adriano <raime910@msn.com>
2017-10-02 01:13:33 -0500

    component selector options

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '.app-servers', using CSS class as selector
+  // selector: '[app-servers]', using DOM attribute as selector
   selector: 'app-servers',
   // templateUrl: './servers.component.html',
   template: `
