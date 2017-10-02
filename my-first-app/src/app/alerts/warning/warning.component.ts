575f01bd9d1571190cd3d3d34671523d69511ac0
Ryan Adriano <raime910@msn.com>
2017-10-02 01:28:09 -0500

    create success and warning components

---
+++ my-first-app/src/app/alerts/warning/warning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning.component.html',
+  styleUrls: ['./warning.component.css']
+})
+export class WarningAlertComponent {
+  message = 'Warning alert message.';
+}
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SuccessAlertComponent } from './alerts/success/success.component';
+import { WarningAlertComponent } from './alerts/warning/warning.component';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -12,7 +14,10 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    // alerts
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
